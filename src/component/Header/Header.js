d8680b7ad43d9dcc15f7504fb4b00622cfd04a41
harrisbang2 <harrisbang2@gmail.com>
2024-09-25 19:26:32 +0900

    Folder organization, sidebar home login.js init

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -26,7 +26,7 @@ function Header() {
             <span className="header__optionLineOne">Hello</span>
             <span className="header__optionLineTwo">Sign In</span>
           </div>
-        
+    
 
         <div className="header__option">
           <span className="header__optionLineOne">Your</span>
